c41979e5272b5febe63df2f209f1db74366344d8
Zekun Shen <17345356+buszk@users.noreply.github.com>
2020-10-28 17:29:01 +0000

    helper for complex module init

---
+++ snapshot_helper_complex.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from common import *
+import subprocess
+import argparse
+from os.path import isfile
+
+import time
+from drifuzz_util import GlobalModel, CommandHandler, SocketThread, qemu_socket
+
+parser = argparse.ArgumentParser()
+parser.add_argument('target', type=str)
+parser.add_argument('--raw', type=str)
+parser.add_argument('qcow', type=str)
+args = parser.parse_args()
+
+if not isfile(args.qcow) and not isfile(args.raw):
+    print(f'{args.qcow} does not exist, must provide raw img with --raw')
+    import sys
+    sys.exit(1)
+
+if not isfile(args.qcow):
+    qemu_img_path = f"{PANDA_BUILD}/qemu-img"
+
+    cmd = [qemu_img_path]
+    cmd += ['convert', '-f', 'raw', '-O', 'qcow2', args.raw, args.qcow]
+    subprocess.check_call(cmd)
+
+global_module = GlobalModel()
+command_handler = CommandHandler(global_module)
+socket_thread = SocketThread(command_handler, qemu_socket)
+
+socket_thread.start()
+
+time.sleep(.1)
+cmd = [qemu_path]
+cmd += [args.qcow]
+cmd += get_extra_args(args.target,socket=qemu_socket)
+
+print(" ".join(cmd))
+subprocess.check_call(cmd)
+
+socket_thread.stop()
