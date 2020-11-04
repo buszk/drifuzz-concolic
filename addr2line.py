9ddefb0080f17585c0696f4dd20aa3d72ed4e296
Zekun Shen <17345356+buszk@users.noreply.github.com>
2020-11-04 22:58:40 +0000

    feature: addr2line for vmlinux too

--- addr2line.py
+++ addr2line.py
@@ -23,22 +23,26 @@ tree.addi(0xffffffffa0018000, 0xffffffffa0018000 + 1642496, "ath10k_core")
 tree.addi(0xffffffffa0000000, 0xffffffffa0000000 + 90112, "ath")
 
 
-assert(len(tree[addr]) == 1)
-
-module_name = list(tree[addr])[0][2]
-module_base = list(tree[addr])[0][0]
-print(f"module_name: {module_name}")
-print(f"module_base: {module_base}")
-
 home = expanduser("~")
 linux_build = join(home, "Workspace", "git", "Drifuzz", "linux-module-build")
-p = subprocess.Popen(['find', linux_build, '-iname', f'{module_name}.ko'], 
-                        stdout=subprocess.PIPE)
-module_path = str(p.communicate()[0], encoding='utf-8')[:-1]
-print(f"module_path: {module_path}")
 
-offset = hex(addr - module_base)
-cmd = ['addr2line', '-f', '-i', '-e', module_path, '-a', offset]
-print(' '.join(cmd))
-p = subprocess.Popen(cmd)
-p.wait()
+if (len(tree[addr]) == 1):
+    module_name = list(tree[addr])[0][2]
+    module_base = list(tree[addr])[0][0]
+    print(f"module_name: {module_name}")
+    print(f"module_base: {module_base}")
+
+    p = subprocess.Popen(['find', linux_build, '-iname', f'{module_name}.ko'], 
+                            stdout=subprocess.PIPE)
+    module_path = str(p.communicate()[0], encoding='utf-8')[:-1]
+    print(f"module_path: {module_path}")
+
+    offset = hex(addr - module_base)
+    cmd = ['addr2line', '-f', '-i', '-e', module_path, '-a', offset]
+    print(' '.join(cmd))
+    p = subprocess.Popen(cmd)
+    p.wait()
+else:
+    cmd = ['addr2line', '-f', '-e', join(linux_build, 'vmlinux'), '-a', f'0x{hex(addr)}']
+    p = subprocess.Popen(cmd)
+    p.wait()
