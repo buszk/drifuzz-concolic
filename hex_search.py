413a4c33fcb401b2f6ce19bd3e99c594abf61163
Zekun Shen <17345356+buszk@users.noreply.github.com>
2021-02-04 20:06:58 +0000

    add hex search unitily script

---
+++ hex_search.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import struct
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=str)
+parser.add_argument('seq', type=str)
+args = parser.parse_args()
+
+odd_n = (len(args.seq) % 2) == 1
+search_bytes = bytearray()
+ind = 0
+for i in range((len(args.seq)+1)//2):
+    byte = 0
+    if i == 0 and odd_n:
+        byte = int(args.seq[ind], 16)
+        ind += 1
+    else:
+        byte += int(args.seq[ind], 16) * 16
+        byte += int(args.seq[ind+1], 16)
+        ind += 2
+    search_bytes = bytearray([byte]) + search_bytes
+
+
+assert os.path.exists(args.file)
+with open(args.file, 'rb') as f:
+    bs = f.read()
+    res = bs.find(search_bytes)
+    print(hex(res))
+
