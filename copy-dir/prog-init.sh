a45e6a6d1c655dd091b27ed8bd1bd39480f69fc4
Zekun Shen <17345356+buszk@users.noreply.github.com>
2021-03-10 13:44:57 +0000

    update driver file

--- copy-dir/prog-init.sh
+++ copy-dir/prog-init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 modprobe $1
+if ip link | grep "eth0"; then
+ip link set dev eth0 up
+elif ip link | grep "wlan0"; then
+ip link set dev wlan0 up
+fi
 cat /proc/modules
-sleep 0.2
+sleep 1
