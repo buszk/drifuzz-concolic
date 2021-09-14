39e3f61e7f340a688fd18095c4fa17bc74cd79e9
buszk <bruceshenzk@gmail.com>
2021-09-13 21:59:13 -0400

    Add default single thread mode

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 concolic_dir=$(dirname $(dirname "$0"))
+parallel=0
+if [ "$1" == "--parallel" ]; then
+    parallel=1
+fi
 
 function wait_ex {
     # this waits for all jobs and returns the exit code of the last failing job
@@ -16,22 +20,28 @@ function wait_ex {
 
 function run {
     echo "Generate snapshot: $1"
-    $concolic_dir/snapshot_helper.py $1 &>/dev/null
+    $concolic_dir/snapshot_helper.py $1 &>$concolic_dir/work/$1/snapshot.log
     echo "Explore: $1"
     $concolic_dir/scripts/run.sh $1 &>/dev/null
     if grep "address: 88," $concolic_dir/work/$1/drifuzz_index >/dev/null; then
         echo "Success: $1"
-        exit 0
+        [ $2 -eq 1 ] && exit 0
     else
         echo "Fail: $1"
-        exit 1
+        [ $2 -eq 1 ] && exit 1
     fi
 }
 
-run drifuzz-test-0 &
-run drifuzz-test-1 &
-run drifuzz-test-2 &
-run drifuzz-test-3 &
-run drifuzz-test-4 &
+num_tests=5
+for i in $(seq 0 $((num_tests - 1))); do
+    if [ ${parallel} -eq 1 ]; then
+        run drifuzz-test-${i} 1 &
+    else
+        run drifuzz-test-${i} 0
+        wait -n
+    fi
+done
 
-wait_ex
+if [ ${parallel} -eq 1 ]; then
+    wait_ex
+fi
