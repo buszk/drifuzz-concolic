1f41da19d6a79f0fbd415eb8f9a9c37eb2a297e0
Zekun Shen <bruceshenzk@gmail.com>
2022-06-08 17:40:21 -0400

    Removing unused files

--- scripts/resume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# != 1 ]; then
-    echo "$0 <target>"
-    exit
-fi
-target=$1
-time ./search_group.py --resume $target random_seed 2>&1|tee -i log/search_group.$target.resume.log
--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# != 1 ]; then
-    echo "$0 <target>"
-    exit
-fi
-mkdir -p log
-target=$1
-time ./search_group.py $target random_seed 2>&1|tee -i log/search_group.$target.log
